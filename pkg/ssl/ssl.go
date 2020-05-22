e128f0635c7a7c9f9f21eb5d89c16875734aab91
Srdjan Grubor <sgnn7@sgnn7.org>
2020-05-22 12:22:48 -0500

    Added ability to extract the cert chain from the target server
    This now does the basic functionality of getting the certificate from
    the target server.

--- pkg/ssl/ssl.go
+++ pkg/ssl/ssl.go
@@ -1,11 +1,41 @@
 package ssl
 
 import (
-	"errors"
+	"bytes"
+	"crypto/tls"
+	"encoding/pem"
+	"log"
+	"net"
 
 	"github.com/sgnn7/crtool/pkg/certificates"
 )
 
+var InsecureTLSConfig = &tls.Config{
+	InsecureSkipVerify: true,
+}
+
 func GetServerCertificate(host string, port string, certType certificates.CertType) error {
-	return errors.New("IMPLEMENT ME")
+	target := net.JoinHostPort(host, port)
+	log.Printf("Dialing '%s'...", target)
+
+	conn, err := tls.Dial("tcp", target, InsecureTLSConfig)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	log.Printf("Connection established")
+
+	var buf bytes.Buffer
+	for _, cert := range conn.ConnectionState().PeerCertificates {
+		err := pem.Encode(&buf, &pem.Block{
+			Bytes: cert.Raw,
+			Type:  "CERTIFICATE",
+		})
+		if err != nil {
+			return err
+		}
+	}
+	log.Printf(buf.String())
+
+	return nil
 }
